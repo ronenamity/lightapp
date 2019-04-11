2f99f0e037c01b58d7cff529b136d91ce0f4d81d
Ronen Amity <amityr@gmail.com>
2019-04-11 14:38:45 +0300

    testing webhook

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Lightapp World 3rd !\n');
+  res.end('Hello Lightapp World 4th !\n');
 });
 
 server.listen(port, () => {
